0fae440a6a6ed58a1a7480b638ce48eada17db3d
Youssef <youssef.b.air2@gmail.com>
2021-12-26 21:34:55 +0100

    Initial Commit

---
+++ SlotsGame/SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsGameApp.swift
+//  SlotsGame
+//
+//  Created by Youssef Bhl on 26/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
